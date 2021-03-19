e31d0150145df0ce1d2c843aab9cbb197fb36c5f
Muhammad-Maher <muhammadahmedmaher@gmail.com>
2021-03-19 21:23:33 +0200

    edit importing the model2

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import { servserResponsep, Product } from '../models/product.model';
+import { servserResponsep, Product } from '../Models/product.model';
 
 @Injectable({
   providedIn: 'root'
