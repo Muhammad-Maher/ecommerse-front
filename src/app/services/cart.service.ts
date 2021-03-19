516ceec48a5341a364cb19f4a591387d382e407c
Muhammad-Maher <muhammadahmedmaher@gmail.com>
2021-03-19 23:16:02 +0200

    after cart creation

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams  } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import{Cart} from '../Models/cart.model';
@@ -9,12 +9,39 @@ import{Cart} from '../Models/cart.model';
 })
 export class CartService {
 
-  constructor(private http: HttpClient) {  }
-  private baseURL:string = `https://ecommerce-food.herokuapp.com/api/cart`;  
+  constructor(private http: HttpClient ) {  }
+  // private baseURL:string = `https://ecommerce-food.herokuapp.com/api/cart`;  
+  private baseURL:string = `http://localhost:3000/api/cart`;  
   
-  
-  getCart(){
-    return this.http.get<Cart>(this.baseURL+`/get`);
+  getId()
+  {
+      const Id=localStorage.getItem("id");
+      if(Id==null)
+      {
+          return ' ';
+      }
+      else{
+          return Id;
+      }
+  }
+
+  getToken()
+{
+    const token=localStorage.getItem("token");
+    if(token==null)
+    {
+        return ' ';
+    }
+    else{
+        return token;
+    }
+}
+
+
+
+getCart(){
+    
+    return this.http.get<Cart>(this.baseURL+`/get/${this.getId()}`,{headers:{authorization:this.getToken()}});
   }    
 
 
