1fde0f482f12b9ed56efdcec5cd0f6226759494e
Muhammad Maher <68520929+Muhammad-Maher@users.noreply.github.com>
2021-05-28 05:35:59 +0200

    change title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,83 +1,74 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 
 //components
 
-import { ErrorComponent } from './components/error/error.component';//*
-import { PromotionsComponent } from './components/promotions/promotions.component';//*
-import { AboutComponent } from './components/about/about.component';//*
-import { AddProductComponent } from './components/add-product/add-product.component';//*
-import { DashboardComponent } from './components/dashboard/dashboard.component';//*
-import { AllProductsComponent } from './components/all-products/all-products.component';//*
-import { CartComponent } from './components/cart/cart.component'//*
-import { DeleteProductComponent } from './components/delete-product/delete-product.component'//*
+import { ErrorComponent } from './components/error/error.component'; //*
+import { PromotionsComponent } from './components/promotions/promotions.component'; //*
+import { AboutComponent } from './components/about/about.component'; //*
+import { AddProductComponent } from './components/add-product/add-product.component'; //*
+import { DashboardComponent } from './components/dashboard/dashboard.component'; //*
+import { AllProductsComponent } from './components/all-products/all-products.component'; //*
+import { CartComponent } from './components/cart/cart.component'; //*
+import { DeleteProductComponent } from './components/delete-product/delete-product.component'; //*
 
 //
-
 
 //alaa
 
-import { HeaderComponent } from './components/header/header.component';//*
-import { FooterComponent } from './components/footer/footer.component';//*
-import { HomeComponent } from './components/home/home.component';//*
-import { SignUpComponent } from './components/sign-up/sign-up.component';//*
-import { LogInComponent } from './components/log-in/log-in.component';//*
-import { ProfileComponent } from './components/profile/profile.component';//*
-import { ProductComponent } from './components/product/product.component';//*
-import{UserOrdersComponent}from './components/user-orders/user-orders.component'//*
-import{ProfileStartComponent} from './components/profile-start/profile-start.component';
-import{EditProfileComponent} from './components/edit-profile/edit-profile.component';
-import{SearchComponent} from './components/search/search.component';
-import{ModifyproductComponent} from './components/modifyproduct/modifyproduct.component';
+import { HeaderComponent } from './components/header/header.component'; //*
+import { FooterComponent } from './components/footer/footer.component'; //*
+import { HomeComponent } from './components/home/home.component'; //*
+import { SignUpComponent } from './components/sign-up/sign-up.component'; //*
+import { LogInComponent } from './components/log-in/log-in.component'; //*
+import { ProfileComponent } from './components/profile/profile.component'; //*
+import { ProductComponent } from './components/product/product.component'; //*
+import { UserOrdersComponent } from './components/user-orders/user-orders.component'; //*
+import { ProfileStartComponent } from './components/profile-start/profile-start.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+import { SearchComponent } from './components/search/search.component';
+import { ModifyproductComponent } from './components/modifyproduct/modifyproduct.component';
 //
 
-
-import{AllusersOrdersComponent}from'./components/allusers-orders/allusers-orders.component'
-
-
-
-
-
-
-
-
-
+import { AllusersOrdersComponent } from './components/allusers-orders/allusers-orders.component';
 
 const routes: Routes = [
   //alaa
-    {path:'',  redirectTo: '/allProducts',  pathMatch: 'full'},//Y
-  {path:'signup',component:SignUpComponent},//Y
-  {path:'login',component:LogInComponent},//Y
+  { path: '', redirectTo: '/allProducts', pathMatch: 'full' }, //Y
+  { path: 'signup', component: SignUpComponent }, //Y
+  { path: 'login', component: LogInComponent }, //Y
   //alaa
   //maher
-  {path:'addproduct',component:AddProductComponent},
-  {path:'about',component:AboutComponent},//Y
-  {path:'product/:id',component:ProductComponent},
-  {path:'allProducts',component:AllProductsComponent},//Y
-  {path:'cart',component:CartComponent},//Y
-  {path:'userorder',component:UserOrdersComponent},
-  {path:'search',component:SearchComponent},
-  {path:'modifyProduct',component:ModifyproductComponent},
-  {path:'DeleteProduct',component:DeleteProductComponent},
+  { path: 'addproduct', component: AddProductComponent },
+  { path: 'about', component: AboutComponent }, //Y
+  { path: 'product/:id', component: ProductComponent },
+  { path: 'allProducts', component: AllProductsComponent }, //Y
+  { path: 'cart', component: CartComponent }, //Y
+  { path: 'userorder', component: UserOrdersComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'modifyProduct', component: ModifyproductComponent },
+  { path: 'DeleteProduct', component: DeleteProductComponent },
   //
-  {path:'profile',component:ProfileComponent,children:[
-    {path:'', component:ProfileStartComponent},
-    {path:'edit', component:EditProfileComponent},
-    {path:'myOrders', component:UserOrdersComponent},
-    {path:'allOrders', component:AllusersOrdersComponent},
-  ]},
-    { path: 'promotions', component: PromotionsComponent },
-    { path: '**', component: ErrorComponent },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    children: [
+      { path: '', component: ProfileStartComponent },
+      { path: 'edit', component: EditProfileComponent },
+      { path: 'myOrders', component: UserOrdersComponent },
+      { path: 'allOrders', component: AllusersOrdersComponent },
+    ],
+  },
+  { path: 'promotions', component: PromotionsComponent },
+  { path: '**', component: ErrorComponent },
 ]; // sets up routes constant where you define your routes
 
 // configures NgModule imports and exports
-@NgModule({ 
-    declarations: [],   
-  imports: [
-    RouterModule,    
-      RouterModule.forRoot(routes)],
-      exports: [RouterModule]
+@NgModule({
+  declarations: [],
+  imports: [RouterModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,61 +3,66 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AppComponent } from '../../app.component';
-import { Auth } from '../../services/auth'
+import { Auth } from '../../services/auth';
 import { SearchService } from '../../services/search.service';
 import { CartService } from '../../services/cart.service';
 import { textChangeRangeIsUnchanged } from 'typescript';
-import { filter } from 'rxjs/operators'
-import { profile } from '../../services/profile'
+import { filter } from 'rxjs/operators';
+import { profile } from '../../services/profile';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor(private router: Router, private reload: AppComponent, private myService: Auth, private myservice: SearchService, private cart: CartService, private route: ActivatedRoute, private profile: profile,private translate: TranslateService) {
+  constructor(
+    private router: Router,
+    private reload: AppComponent,
+    private myService: Auth,
+    private myservice: SearchService,
+    private cart: CartService,
+    private route: ActivatedRoute,
+    private profile: profile,
+    private translate: TranslateService
+  ) {
     translate.addLangs(['en', 'ar']);
-   translate.setDefaultLang('en');
-   }
-   useLanguage(language: string) {
+    translate.setDefaultLang('en');
+  }
+  useLanguage(language: string) {
     this.translate.use(language);
   }
-  
-  token = null
-  myToken = null
+
+  token = null;
+  myToken = null;
   LogSub: Subscription;
   cartItems = 0;
   subscriber: Subscription;
   subscriberCartItems: Subscription;
   subscriberAdmin: Subscription;
   name: string;
-  myform: FormGroup
-  value
-  selectedValueOptions
-  adminCheck
-  userProfile
-  username
-  lastname
+  myform: FormGroup;
+  value;
+  selectedValueOptions;
+  adminCheck;
+  userProfile;
+  username;
+  lastname;
   isCollapsed = true;
   toggleCollapsed(): void {
     this.isCollapsed = !this.isCollapsed;
   }
-  
+
   ngOnInit(): void {
-  
-    this.token = localStorage.getItem("token");
-    this.LogSub = this.myService.logedUser.subscribe(res => {
-
-      this.ngOnInit()
-
-    })
+    this.token = localStorage.getItem('token');
+    this.LogSub = this.myService.logedUser.subscribe((res) => {
+      this.ngOnInit();
+    });
 
     this.myform = new FormGroup({
-      'body': new FormControl(null)
-    })
+      body: new FormControl(null),
+    });
 
     if (this.token) {
       this.subscriber = this.cart.getCart().subscribe((val) => {
@@ -68,31 +73,23 @@ export class HeaderComponent implements OnInit {
         this.cart.setCartItems(this.cartItems);
 
         this.subscriberCartItems = this.cart.cartItems.subscribe((val) => {
-          this.cartItems = val
-        })
-
-      })
-
-
+          this.cartItems = val;
+        });
+      });
     }
 
-
-
-    this.profile.getProfile()
-    this.userProfile = this.profile.profile.subscribe(res => {     
-      this.adminCheck = res["userData"].status;   
-      this.username = res["userData"].username;
-      this.lastname = res["userData"].lname;
-    })
-
-
+    this.profile.getProfile();
+    this.userProfile = this.profile.profile.subscribe((res) => {
+      this.adminCheck = res['userData'].status;
+      this.username = res['userData'].username;
+      this.lastname = res['userData'].lname;
+    });
   }
 
-  translateLanguage(selectedlanguage){
+  translateLanguage(selectedlanguage) {
     console.log('here');
     this.translate.use(selectedlanguage);
   }
-
 
   checkToken() {
     if (this.token == null) {
@@ -100,27 +97,19 @@ export class HeaderComponent implements OnInit {
     }
   }
   logOut() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("id");
+    localStorage.removeItem('token');
+    localStorage.removeItem('id');
     this.router.navigate(['/']);
     this.ngOnInit();
   }
 
-
   getProductsByName() {
-
-    
-    this.myservice.getProductByname(this.myform.get('body').value)
-    this.router.navigate(['/search'])
+    this.myservice.getProductByname(this.myform.get('body').value);
+    this.router.navigate(['/search']);
   }
 
   ngOnDestroy() {
     this.subscriber.unsubscribe();
     this.subscriberCartItems.unsubscribe();
   }
-
-
-
-
-
 }
